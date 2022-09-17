baecfcdaee288a00aa123121a2e46db808e10e0e
Denis-Ozolin <ozolindenx@gmail.com>
2022-09-17 19:28:53 +0300

    added controls select

--- db/archivedNotes.js
+++ db/archivedNotes.js
@@ -5,6 +5,12 @@ const archivedNotes = [
     content: 'Get a third dog',
     active: false,
   },
+  {
+    create: 'Dec 31 2021',
+    category: 'Random Thought',
+    content: 'Do not drink alcohol on the night from 12/31/2021 to 01/01/2022',
+    active: false,
+  },
 ];
 
 export default archivedNotes;
--- js/main.js
+++ js/main.js
@@ -3,65 +3,32 @@ import notes from '../db/notes.js';
 import archivedNotes from '../db/archivedNotes.js';
 import categories from './noteCategories.js';
 
-function deleteNote(index) {
-  console.log(index);
+let notesIsActive = true;
+
+function deleteNote(id) {
+  notesIsActive ? notes.splice(id, 1) : archivedNotes.splice(id, 1);
+  resetData();
 }
 
-function test(event) {
+function onClickNoteControls(event) {
   if (event.target.nodeName !== 'BUTTON') return;
+  const title = event.target.innerText;
+  const id = event.currentTarget.id;
 
-  if (event.target.classList.contains('button--edit')) {
-    console.log('btn edit');
-    return;
+  switch (title) {
+    case 'Edit':
+      console.log('btn edit');
+      break;
+    case 'Archive':
+      console.log('btn archive');
+      break;
+    case 'Delete':
+      deleteNote(id);
+      break;
+    default:
+      return;
   }
-
-  if (event.target.classList.contains('button--delete')) {
-    refs.noteList.removeChild();
-    console.log('btn delete');
-    return;
-  }
 }
-
-// TEMPLATES
-function createNoteTemplate(note, index) {
-  const dates = getDates(note.content).join(', ');
-
-  return ` 
-    <li class="notes-list__item">
-      <ul class="note">
-        <li class="note__content">${note.create}</li>
-        <li class="note__content">${note.category}</li>
-        <li class="note__content">${note.content}</li>
-        <li class="note__content">${dates ? dates : ''}</li>
-        <li class="note__content">
-          <div class="controls">
-            <button type="button" class="button button--edit">Edit</button>
-            <button onClick="deleteNote(${index})" type="button" class="button button--delete">Delete</button>
-          </div>
-        </li>
-      </ul>
-    </li>`;
-}
-
-function createStatCtagoryTemplate(category, data) {
-  return `
-    <li class="notes-list__item">
-      <ul class="note">
-        <li class="note__content">${category}</li>
-        <li class="note__content">${data.active}</li>
-        <li class="note__content">${data.archived}</li>
-      </ul>
-    </li>`;
-}
-
-function createDefaultOptionTemplate() {
-  return `<option value="" selected disabled hidden>Choose category</option>`;
-}
-
-function createSelectOptionsTemplate(option) {
-  return `<option value="${option}">${option}</option>`;
-}
-//TEMPLATES
 
 function getData(category) {
   const allNotes = [...notes, ...archivedNotes];
@@ -111,7 +78,7 @@ function renderSelectOptions(options) {
   });
 }
 
-function addNewNote() {
+function addNewNote(event) {
   event.preventDefault();
 
   if (!refs.input.value && !refs.select.value) {
@@ -140,15 +107,89 @@ function addNewNote() {
   notes.push(newNote);
 
   closeModal();
-  updateNotes();
+  resetData();
+}
+
+function toggleArchiveBtnText() {
+  notesIsActive
+    ? (refs.toggleIsOpenArchiveBtn.innerText = 'Open Archive')
+    : (refs.toggleIsOpenArchiveBtn.innerText = 'Close Archive');
+}
+
+function toggleNotesIsActive() {
+  notesIsActive = !notesIsActive;
+  selectNotes();
+  toggleArchiveBtnText();
+}
+
+function selectNotes() {
+  notesIsActive ? updateNotes(notes) : updateNotes(archivedNotes);
+}
+
+function resetData() {
+  selectNotes();
   updateStatistics();
 }
 
-function updateNotes() {
+//TEMPLATES
+function createListItemTemplate(note, index) {
+  const item = document.createElement('li');
+
+  item.className = 'notes-list__item';
+  item.id = index;
+  item.innerHTML = createNoteTemplate(note);
+  item.addEventListener('click', onClickNoteControls);
+
+  return item;
+}
+
+function createNoteTemplate(note) {
+  const dates = getDates(note.content).join(', ');
+
+  return ` 
+      <ul class="note">
+        <li class="note__content">${note.create}</li>
+        <li class="note__content">${note.category}</li>
+        <li class="note__content">${note.content}</li>
+        <li class="note__content">${dates ? dates : ''}</li>
+        <li class="note__content">
+          <div class="controls">
+            <button type="button" class="button button--edit">Edit</button>
+            <button type="button" class="button button--archive">Archive</button>
+            <button type="button" class="button button--delete">Delete</button>
+          </div>
+        </li>
+      </ul>
+      `;
+}
+
+function createStatCtagoryTemplate(category, data) {
+  return `
+    <li class="notes-list__item">
+      <ul class="note">
+        <li class="note__content">${category}</li>
+        <li class="note__content">${data.active}</li>
+        <li class="note__content">${data.archived}</li>
+      </ul>
+    </li>`;
+}
+
+function createDefaultOptionTemplate() {
+  return `<option value="" selected disabled hidden>Choose category</option>`;
+}
+
+function createSelectOptionsTemplate(option) {
+  return `<option value="${option}">${option}</option>`;
+}
+//TEMPLATES
+
+//MARKUP
+function updateNotes(list) {
   refs.noteList.innerHTML = '';
-  if (notes.length > 0) {
-    notes.forEach((note, index) => {
-      refs.noteList.innerHTML += createNoteTemplate(note, index);
+  if (list.length > 0) {
+    list.forEach((note, index) => {
+      const listItem = createListItemTemplate(note, index);
+      refs.noteList.append(listItem);
     });
   }
 }
@@ -162,14 +203,19 @@ function updateStatistics() {
     });
   }
 }
-
-updateNotes();
-updateStatistics();
+//MARKUP
 
 //LISTENERS
 window.addEventListener('keydown', handleKeyDown);
+refs.toggleIsOpenArchiveBtn.addEventListener('click', toggleNotesIsActive);
 refs.modalBackdrop.addEventListener('click', handleBackdropClick);
 refs.createNoteBtn.addEventListener('click', openModal);
 refs.closeModalBtn.addEventListener('click', closeModal);
 refs.addNoteBtn.addEventListener('click', addNewNote);
 //LISTENERS
+
+//DEFAULT CALLS
+// selectNotes();
+// updateStatistics();
+resetData();
+//DEFAULT CALLS
